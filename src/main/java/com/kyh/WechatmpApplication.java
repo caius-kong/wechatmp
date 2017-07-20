c927355e433a48a2e509ce06004c0535b73865f3
孔昀晖 <1032316751@qq.com>
2017-07-20 16:01:36 +0800

    maven project

---
+++ src/main/java/com/kyh/WechatmpApplication.java
@@ -0,0 +1,12 @@
+package com.kyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatmpApplication.class, args);
+	}
+}
